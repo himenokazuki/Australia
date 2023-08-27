4a054856358fea18e6062955c6edb1644db8c4d2
himenokazuki <129943270+himenokazuki@users.noreply.github.com>
2023-08-27 17:31:54 +0000

    PF制作

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+  root to: 'public/homes#top'
+
+  devise_for :admin, controllers: {
+  sessions: "admin/sessions"
+  }
   
-  devise_for :admins
-  devise_for :users
- root to: 'public/homes#top'
+  devise_for :users, controllers: {
+  registrations: "users/registrations",
+  sessions: 'user/sessions'
+  }
+ 
+ 
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
